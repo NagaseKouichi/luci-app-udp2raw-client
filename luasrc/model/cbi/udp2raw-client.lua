10016f534c60312641af495cc3bcbff80ab6f19f
NagaseKouichi <nagaseminato8255@gmail.com>
2025-03-20 17:36:34 +0900

    show status

--- luasrc/model/cbi/udp2raw-client.lua
+++ luasrc/model/cbi/udp2raw-client.lua
@@ -4,54 +4,56 @@
 
 local m, s, o
 
-local raw_modes    = {
+local raw_modes                   = {
     "faketcp",
     "udp",
     "icmp",
 }
 
-local cipher_modes = {
+local cipher_modes                = {
     "aes128cbc",
     "xor",
     "none",
 }
 
-local auth_modes   = {
+local auth_modes                  = {
     "md5",
     "crc32",
     "simple",
     "none",
 }
 
-m                  = Map("udp2raw-client", translate("Udp2raw Client"),
+m                                 = Map("udp2raw-client", translate("Udp2raw Client"),
     translate("A simple Luci webui and shell for Udp2raw that can detect remote server ip change and restart Udp2raw."))
 
-s                  = m:section(TypedSection, "udp2raw-client", translate("General Setting"))
-s.anonymous        = true
+m:section(SimpleSection).template = "udp2raw-client/status"
 
-o                  = s:option(Flag, "enable", translate("Enable"))
-o.rmempty          = false
+s                                 = m:section(TypedSection, "udp2raw-client", translate("General Setting"))
+s.anonymous                       = true
 
-o                  = s:option(Value, "listen_addr", translate("Local Listen Host"))
-o.datatype         = "ipaddr"
-o.placeholder      = "127.0.0.1"
+o                                 = s:option(Flag, "enable", translate("Enable"))
+o.rmempty                         = false
 
-o                  = s:option(Value, "listen_port", translate("Local Listen Port"))
-o.datatype         = "port"
-o.placeholder      = "12345"
+o                                 = s:option(Value, "listen_addr", translate("Local Listen Host"))
+o.datatype                        = "ipaddr"
+o.placeholder                     = "127.0.0.1"
 
-o                  = s:option(Value, "server_addr", translate("Server Domain"))
-o.datatype         = "host"
-o.rmempty          = false
+o                                 = s:option(Value, "listen_port", translate("Local Listen Port"))
+o.datatype                        = "port"
+o.placeholder                     = "12345"
 
-o                  = s:option(Value, "server_port", translate("Server Port"))
-o.datatype         = "port"
-o.placeholder      = "54321"
+o                                 = s:option(Value, "server_addr", translate("Server Domain"))
+o.datatype                        = "host"
+o.rmempty                         = false
 
-o                  = s:option(Value, "key", translate("Password"))
-o.password         = true
+o                                 = s:option(Value, "server_port", translate("Server Port"))
+o.datatype                        = "port"
+o.placeholder                     = "54321"
 
-o                  = s:option(ListValue, "raw_mode", translate("Raw Mode"))
+o                                 = s:option(Value, "key", translate("Password"))
+o.password                        = true
+
+o                                 = s:option(ListValue, "raw_mode", translate("Raw Mode"))
 for _, v in ipairs(raw_modes) do o:value(v, v:lower()) end
 o.default = "faketcp"
 o.rmempty = false
